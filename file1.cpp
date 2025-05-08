a6bec0001d19e7660b9ac507b99b61af4409e8e2
irfanalarib <irfanalarib7@gmail.com>
2025-05-08 11:44:40 +0700

    tambahkan menu interaktif dengan opsi tambah, hapus, tampilkan, cari, dan keluar

--- file1.cpp
+++ file1.cpp
@@ -106,4 +106,19 @@ int main(){
     singleLinkedlist mhs;
     int nim;
     char ch;
+
+    while(1)
+    {
+        cout << endl
+             << "MENU";
+        cout << endl
+             << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukan piliham (1-5): ";
+        cin >> ch;
+}
 }

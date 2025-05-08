9e22d67744c97baae1dd1f7a95b1afef98470c23
irfanalarib <irfanalarib7@gmail.com>
2025-05-08 11:48:58 +0700

    implementasi pencarian data mahasiswa dengan NIM jika ditemukan pada menu pilihan 4

--- file1.cpp
+++ file1.cpp
@@ -162,6 +162,26 @@ int main(){
                 cout << "\nList kosong\n";
                 break;
         }
+        node *previous, *current;
+        cout << endl
+             << "Masukkan no mahasiswa yang dicari : ";
+        cin >> nim;
+        if (mhs.Search(nim, &previous, &current) == false)
+            cout << endl
+                 << " Data tidak ditemukan" << endl;
+                
+                 else
+                 {
+                     cout << endl
+                          << "Data ditemukan" << endl;
+                     cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                     cout << "\n";
+         
+                 }
+         
+                 }
+         
+             break;
     }
 }
 }

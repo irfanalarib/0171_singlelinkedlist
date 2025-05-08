a971af75231651fafdf18762fba72783755c98ec
irfanalarib <irfanalarib7@gmail.com>
2025-05-08 10:38:38 +0700

    menambahkan fungsi while untuk mencegah duplikasi nim den menyisipkan node di posisi yang tepat

--- file1.cpp
+++ file1.cpp
@@ -39,5 +39,19 @@ void addnode(){
     
     node *previous = START;
     node *current = START;
+
+    while ((current != NULL) && (nim >= current ->noMhs))
+    {
+        if (nim == current ->noMhs)
+        {
+            cout << "\nDuplikasi noMhs todak diijinkan\n";
+            return;
+        }
+        previous = current;
+        current = current ->next;
+
+        nodebaru ->next = current;
+        previous->next =nodebaru;
+    }
 }
 };

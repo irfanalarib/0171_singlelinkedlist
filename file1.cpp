0088a2e47693a06523bf5b19a9f930852fc60aa4
irfanalarib <irfanalarib7@gmail.com>
2025-05-08 11:40:43 +0700

    Implementasi fungsi delnode untuk menghapus node berdasarkan NIM

--- file1.cpp
+++ file1.cpp
@@ -71,4 +71,19 @@ bool Search(int nim, node **previous, node **current)
     }
     return (*current != NULL);
 }
+
+bool delnode(int nim){
+    node *current, *previous;
+    if (!Search(nim, &previous, &current))
+    return false;
+
+    if (current == START)
+    START = START->next;
+    else
+
+        previous->next = current->next;
+    
+    delete current;
+    return true;
+}
 };
